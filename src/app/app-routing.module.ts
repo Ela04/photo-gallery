f6611939d77e0919ee0c0ea636655e40ec020436
Ela04 <ga.antunez@duocuc.cl>
2023-11-26 18:49:15 -0300

    delete about

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,11 +7,6 @@ const routes: Routes = [
     loadChildren: () =>
       import('./tabs/tabs.module').then((m) => m.TabsPageModule),
   },
-  {
-    path: 'about',
-    loadChildren: () =>
-      import('./about/about.module').then((m) => m.AboutPageModule),
-  },
 ];
 @NgModule({
   imports: [
